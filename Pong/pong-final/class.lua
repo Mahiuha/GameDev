81fc3be95a7049426da677b519d37b2727977cff
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 21:27:50 +0300

    refactoring code

--- Pong/pong-final/class.lua
+++ Pong/pong-final/class.lua
@@ -13,7 +13,6 @@ all copies or substantial portions of the Software.
 
 Except as contained in this notice, the name(s) of the above copyright holders
 shall not be used in advertising or otherwise to promote the sale, use or
-other dealings in this Software without prior written authorization.
 
 ]]--
 local function include_helper(to, from, seen)

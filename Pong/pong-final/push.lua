ad3eea4f83b6cc908fe65957c60a34e687dc726d
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 21:20:32 +0300

    Bug fix

--- Pong/pong-final/push.lua
+++ Pong/pong-final/push.lua
@@ -1,7 +1,7 @@
 -- push.lua v0.4
 
 -- Copyright (c) 2020 Joseph Mahiuha.
--- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
 local love11 = love.getVersion() == 11
 local getDPI = love11 and love.window.getDPIScale or love.window.getPixelScale
 local windowUpdateMode = love11 and love.window.updateMode or function(width, height, settings)

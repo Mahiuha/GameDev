38e3c5b89f661bc183e121c4cd3d42c0a3f464af
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 21:29:30 +0300

    refactoring code

--- Pong/pong-final/class.lua
+++ Pong/pong-final/class.lua
@@ -8,7 +8,6 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-
 ]]--
 local function include_helper(to, from, seen)
 	if from == nil then

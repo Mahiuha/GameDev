2f707b0f8c48bee0468a70363fd9fd10df7f5f82
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 21:39:57 +0300

    refactoring code

--- Pong/pong-12/Paddle.lua
+++ Pong/pong-12/Paddle.lua
@@ -5,7 +5,6 @@
     -- Paddle Class --
 
     Author: Colton Ogden
-    cogden@cs50.harvard.edu
 
     Represents a paddle that can move up and down. Used in the main
     program to deflect the ball back toward the opponent.

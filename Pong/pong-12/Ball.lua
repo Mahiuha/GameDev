04c38212aa7acde89b70048e7fae21830f60901e
Mahiuha <tiasho.mahiuha@gmail.com>
2021-12-31 21:38:52 +0300

    refactoring code

--- Pong/pong-12/Ball.lua
+++ Pong/pong-12/Ball.lua
@@ -5,7 +5,6 @@
     -- Ball Class --
 
     Author: Colton Ogden
-    cogden@cs50.harvard.edu
 
     Represents a ball which will bounce back and forth between paddles
     and walls until it passes a left or right boundary of the screen,
